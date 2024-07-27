9f7201dd4158dfde3a426e0b6b51a5f0b22859da
aazyablicev <aazyablicev@gmail.com>
2024-07-27 16:33:39 +0300

    project

---
+++ project/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  description = "The GCP project ID"
+}
+
+variable "region" {
+  description = "The GCP region"
+  default     = "europe-west2"
+}
+
+variable "zone" {
+  description = "The GCP zone"
+  default     = "europe-west2-a"
+}
