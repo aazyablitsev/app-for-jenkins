23e867355875f63effe2649178460c3a7958e7ee
aazyablitsev <154759712+aazyablitsev@users.noreply.github.com>
2024-07-27 11:38:39 +0300

    Update startup.sh

--- project/scripts/startup.sh
+++ project/scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y docker.io docker-compose
+sudo usermod -aG docker aazyablicev
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker aazyablicev
