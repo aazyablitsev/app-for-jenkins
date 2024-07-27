ce8707837eea50e9c4fc6c24302b352a44c10468
aazyablitsev <154759712+aazyablitsev@users.noreply.github.com>
2024-07-27 21:55:44 +0300

    Update startup.sh

--- project/scripts/startup.sh
+++ project/scripts/startup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
 sudo apt-get update
-sudo apt-get install -y docker.io docker-compose
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d'"' -f4)
+sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo usermod -aG docker aazyablicev
 sudo systemctl start docker
 sudo systemctl enable docker
